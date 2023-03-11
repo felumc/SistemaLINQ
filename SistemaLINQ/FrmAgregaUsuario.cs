6b11c1cad98e431e13138dcc4d2236b1f713141d
Luis Morales <luismc2211@gmail.com>
2023-03-10 19:17:33 -0600

    DataClasses1 update and fixing

--- SistemaLINQ/FrmAgregaUsuario.cs
+++ SistemaLINQ/FrmAgregaUsuario.cs
@@ -1,21 +1,27 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Protocols;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
 
 namespace SistemaLINQ
 {
+    
     public partial class FrmAgregaUsuario : Form
     {
-        
+        DataClasses1DataContext dataContext;
         public FrmAgregaUsuario()
         {
             InitializeComponent();
+            string Conexion = ConfigurationManager.ConnectionStrings["SistemaLINQ.Properties.Settings.ejercicioConnectionString"].ConnectionString;
+            dataContext = new DataClasses1DataContext(Conexion);
         }
 
         private void FrmAgregaUsuario_FormClosed(object sender, FormClosedEventArgs e)
