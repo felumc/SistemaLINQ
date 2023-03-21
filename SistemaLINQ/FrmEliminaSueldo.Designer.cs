29593459d6b1d68b69a472658ed94d3d18863661
Luis Morales <luismc2211@gmail.com>
2023-03-21 16:05:05 -0600

    Eliminar Sueldos Funcional

--- SistemaLINQ/FrmEliminaSueldo.Designer.cs
+++ SistemaLINQ/FrmEliminaSueldo.Designer.cs
@@ -28,20 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbSueldos = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(35, 134);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(115, 45);
+            this.btnEliminar.TabIndex = 26;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(315, 134);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(115, 44);
+            this.btnCerrar.TabIndex = 25;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Sueldos:";
+            // 
+            // cmbSueldos
+            // 
+            this.cmbSueldos.FormattingEnabled = true;
+            this.cmbSueldos.Location = new System.Drawing.Point(176, 42);
+            this.cmbSueldos.Name = "cmbSueldos";
+            this.cmbSueldos.Size = new System.Drawing.Size(254, 24);
+            this.cmbSueldos.TabIndex = 23;
             // 
             // FrmEliminaSueldo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(487, 220);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbSueldos);
             this.Name = "FrmEliminaSueldo";
             this.Text = "FrmEliminaSueldo";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmEliminaSueldo_FormClosed);
             this.Load += new System.EventHandler(this.FrmEliminaSueldo_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbSueldos;
     }
 }
--- SistemaLINQ/FrmEliminaSueldo.cs
+++ SistemaLINQ/FrmEliminaSueldo.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Protocols;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -12,14 +14,48 @@ namespace SistemaLINQ
 {
     public partial class FrmEliminaSueldo : Form
     {
+        DataClasses1DataContext dataContext;
+        public List<sueldo> listaSueldos;
         public FrmEliminaSueldo()
         {
             InitializeComponent();
+            string Conexion = ConfigurationManager.ConnectionStrings["SistemaLINQ.Properties.Settings.ejercicioConnectionString"].ConnectionString;
+            dataContext = new DataClasses1DataContext(Conexion);
         }
 
         private void FrmEliminaSueldo_Load(object sender, EventArgs e)
         {
+            //List all the sueldos on cmbSueldos
+            var lista = dataContext.sueldo.OrderBy(x => x.id_sueldo).ToList();
+            foreach (sueldo sueldo in lista)
+            {
+                cmbSueldos.Items.Add(sueldo.id_sueldo);
+            }
+            
+        }
 
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //Delete the selected sueldo from the cmbSueldos
+            sueldo sueldo = dataContext.sueldo.FirstOrDefault(sueld => sueld.id_sueldo.Equals(cmbSueldos.SelectedItem.ToString()));
+            dataContext.sueldo.DeleteOnSubmit(sueldo);
+            dataContext.SubmitChanges();
+            MessageBox.Show("Sueldo eliminado");
+            cmbSueldos.Items.Clear();
+            cmbSueldos.Text = "";
+            FrmEliminaSueldo_Load(sender, e);
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            FrmPrincipal frmPrincipal = new FrmPrincipal();
+            frmPrincipal.Show();
+            this.Hide();
+        }
+
+        private void FrmEliminaSueldo_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            btnCerrar_Click(sender, e);
         }
     }
 }
