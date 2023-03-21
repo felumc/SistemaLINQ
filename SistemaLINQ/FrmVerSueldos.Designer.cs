c7921f583de79ad1afe1458772e6d905546412b7
Luis Morales <luismc2211@gmail.com>
2023-03-21 16:15:15 -0600

    Ver sueldos funcional

--- SistemaLINQ/FrmVerSueldos.Designer.cs
+++ SistemaLINQ/FrmVerSueldos.Designer.cs
@@ -28,12 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(304, 385);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(115, 44);
+            this.btnCerrar.TabIndex = 22;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(688, 353);
+            this.dataGridView1.TabIndex = 21;
+            // 
+            // FrmVerSueldos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(717, 441);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FrmVerSueldos";
             this.Text = "FrmVerSueldos";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmVerSueldos_FormClosed);
+            this.Load += new System.EventHandler(this.FrmVerSueldos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- SistemaLINQ/FrmVerSueldos.cs
+++ SistemaLINQ/FrmVerSueldos.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Protocols;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -12,9 +14,39 @@ namespace SistemaLINQ
 {
     public partial class FrmVerSueldos : Form
     {
+        DataClasses1DataContext dataContext;
+        public List<sueldo> listaSueldos;
         public FrmVerSueldos()
         {
             InitializeComponent();
+            string Conexion = ConfigurationManager.ConnectionStrings["SistemaLINQ.Properties.Settings.ejercicioConnectionString"].ConnectionString;
+            dataContext = new DataClasses1DataContext(Conexion);
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            FrmPrincipal frmPrincipal = new FrmPrincipal();
+            frmPrincipal.Show();
+            this.Hide();
+        }
+
+        private void FrmVerSueldos_Load(object sender, EventArgs e)
+        {
+            //Add the sueldos from dataContext to the DataGridView
+            var lista = dataContext.sueldo.OrderBy(x => x.id_sueldo).ToList();
+            dataGridView1.Columns.Add("id_sueldo", "ID");
+            dataGridView1.Columns.Add("id_trabajador", "ID Trabajador");
+            dataGridView1.Columns.Add("id_puesto", "ID Puesto");
+            dataGridView1.Columns.Add("sueldo", "Sueldo");
+            foreach (sueldo sueldo in lista)
+            {
+                dataGridView1.Rows.Add(sueldo.id_sueldo, sueldo.id_trabajador, sueldo.id_puesto, sueldo.sueldo1);
+            }
+        }
+
+        private void FrmVerSueldos_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            btnCerrar_Click(sender, e);
         }
     }
 }
